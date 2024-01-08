ac152181385bebead5536ae13eae6d83934ab463
Saurlax <saurlax@qq.com>
2024-01-08 20:41:05 +0800

    feat: new cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,9 +1,6 @@
-import { globSync } from 'glob'
 import Vivia from '../vivia.js'
 
-// ;(async () => {
-//   console.log(globSync('**/*.md', { cwd: 'source' }))
-// })()
 const vivia = new Vivia(process.cwd())
-// // await vivia.init()
+await vivia.init()
 await vivia.render()
+console.log('done')
